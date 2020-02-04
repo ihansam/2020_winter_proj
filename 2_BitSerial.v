7afdf82c43a8bc94a3b3b358d39dbb7ae05442a8
KidanJIN <ihansam@skku.edu>
2020-02-04 14:40:53 +0900

    bit serial complete

--- 2_BitSerial.v
+++ 2_BitSerial.v
@@ -5,7 +5,7 @@ module MAC_Unit(
     output reg [2:0] count,
     output Win,
     output [7:0] WA,
-    output done, triger,
+    output done, trigger,
     output [8:0] op1, op2,
     output [8:0] partsumres,
     output reg [15:0] PRODUCT,
@@ -21,8 +21,8 @@ module MAC_Unit(
         end
     end
 
-    // triger = 1 when last bit of weight enter, done = 1 when partial sum done 
-    assign triger = count[0] & ((count[1]&ReducePrecLevel[0])|(count[2]&count[1])|(ReducePrecLevel[1]&~ReducePrecLevel[0]));
+    // trigger = 1 when last bit of weight enter, done = 1 when partial sum done 
+    assign trigger = count[0] & ((count[1]&ReducePrecLevel[0])|(count[2]&count[1])|(ReducePrecLevel[1]&~ReducePrecLevel[0]));
     assign done = ~count[0] & ((~count[1]&ReducePrecLevel[0])|(~count[2]&~count[1])|(ReducePrecLevel[1]&~ReducePrecLevel[0]));
 
     // calculate weight bit * Activation
@@ -42,7 +42,7 @@ module MAC_Unit(
     end
     generate
         for(i=0; i<8; i=i+1) begin
-            MUX_2 mux (WA[i], WAprime[i], triger, op1[i]);
+            MUX_2 mux (WA[i], WAprime[i], trigger, op1[i]);
         end
     endgenerate
     assign op1[8] = op1[7];
@@ -57,7 +57,7 @@ module MAC_Unit(
     
     // partial sum part
     // wire [8:0] partsumres;
-    ADDER #(.size(9)) productACCUM (op1, op2, triger, partsumres);
+    ADDER #(.size(9)) productACCUM (op1, op2, trigger, partsumres);
 
     // partial sum register
     wire [15:0] dPRODUCT = {partsumres, prevProduct[7:1]};
@@ -72,8 +72,17 @@ module MAC_Unit(
     wire [19:0] accold, accnew, accumres;
     assign accold = RESULT;
     assign accnew = {{4{PRODUCT[15]}}, PRODUCT};
-    assign accumres = accold + accnew;
-    always @(posedge done, negedge rstn) begin
+    //accumres = accold + accnew
+    wire [2:1] CIN;
+    wire [2:0] COUT;
+    ADDERc #(4) acc0 (accold[3:0], accnew[3:0], 0, accumres[3:0], COUT[0]);
+    ADDERc #(2) acc1 (accold[5:4], accnew[5:4], CIN[1], accumres[5:4], COUT[1]);
+    ADDERc #(14) acc2 (accold[19:6], accnew[19:6], CIN[2], accumres[19:6], COUT[2]);
+    
+    MUX_2 cin2control (COUT[1], 0, ReducePrecLevel[1], CIN[2]);
+    MUX_2 cin1control (COUT[0], 0, ReducePrecLevel[0], CIN[1]);
+       
+    always @(negedge done, negedge rstn) begin
         if(rstn == 0)
             RESULT = 0;
         else if (en == 1)
@@ -93,6 +102,26 @@ module MUX_2(
     nand(bs, B, sel);
     nand(O, asp, bs);     
     
+endmodule
+
+module ADDERc(op1, op2, cin, res, cout);     // with carry out
+    parameter size = 12;
+    input [size-1:0] op1, op2;
+    input cin;
+    output [size-1:0] res;
+    output cout;
+    wire [size:0] C;
+
+    genvar i;
+    assign C[0] = cin;
+    assign cout = C[size];
+
+    generate
+        for(i=0; i<size; i=i+1) begin
+            fullAdder FAwc (.A(op1[i]), .B(op2[i]), .ci(C[i]), .s(res[i]), .co(C[i+1]));
+        end
+    endgenerate
+
 endmodule
 
 module ADDER(op1, op2, cin, res);           // without carry out
@@ -131,20 +160,24 @@ endmodule
 module tb_bitserial();
     reg [7:0] A, W;
     reg clk, rstn, en;
-    reg [1:0] Precesion;
+    reg [1:0] Precision;
     wire [2:0] count;
     wire win;
     wire [7:0] WA;
-    wire done, triger;
+    wire done, trigger;
     wire [8:0] op1, op2;
     wire [8:0] ptsum;
     wire [15:0] PRODUCT;
-    wire [19:0] ACCUM;    
+    wire [19:0] ACCUM;
+    wire [15:4] PRODUCT4b = PRODUCT[15:4];
+    wire [19:4] ACC4b = ACCUM[19:4];
+    wire [15:6] PRODUCT2b = PRODUCT[15:6];
+    wire [19:6] ACC2b = ACCUM[19:6];    
 
-    MAC_Unit MAC (A, W, clk, rstn, en, Precesion, count, win, WA, done, triger, op1, op2, ptsum, PRODUCT, ACCUM);
+    MAC_Unit MAC (A, W, clk, rstn, en, Precision, count, win, WA, done, trigger, op1, op2, ptsum, PRODUCT, ACCUM);
 
     initial begin
-        #0  clk = 0; rstn = 0; en = 0; Precesion = 2'b00; A = 0; W = 0;
+        #0  clk = 0; rstn = 0; en = 0; Precision = 2'b00; A = 0; W = 0;
         #10 rstn = 1;
         #5  A = 8'b01100111; W = 8'b00001010;
         #5  en = 1;
@@ -152,7 +185,16 @@ module tb_bitserial();
         #80 A = 8'b10110100; W = 8'b01000000;
         #80 A = 8'b10101001; W = 8'b10110101;        
         #80 A = 8'b10000000; W = 8'b10000000;        
-        #85  en = 0;
+        #95 en = 0; rstn = 0; Precision = 2'b01; A = 0; W = 0;
+        #10 rstn = 1; A = 8'b01111011; W = 8'b0100_1000;
+        #10  en = 1;
+        #75 A = 8'b10011000; W = 8'b01111011;
+        #95 en = 0; rstn = 0; Precision = 2'b10; A = 0; W = 0;
+        #10 rstn = 1; A = 8'b01011100; W = 8'b01_00_11_10;
+        #10 en = 1;
+        #75 A = 8'b11010001; W = 8'b10_00_11_01;
+        #95 en = 0;
+        
     end
 
     always #5 clk = ~clk;
